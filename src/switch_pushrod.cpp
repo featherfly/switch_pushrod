9aa9b132f6cbd3e1c62d525c64b95ec6e1aa9883
zhongji <featherfly@foxmail.com>
2020-10-22 15:24:57 +0800

    引入步进事件

--- src/switch_pushrod.cpp
+++ src/switch_pushrod.cpp
@@ -2,14 +2,14 @@
 
 SwitchPushrod::SwitchPushrod() {}
 
-SwitchPushrod::SwitchPushrod(Pushrod *_pushrod, Switch &_start, Switch &_end)
+SwitchPushrod::SwitchPushrod(Pushrod *_pushrod, Switch *_start, Switch *_end)
 {
     pushrod = _pushrod;
     start = _start;
     end = _end;
 }
 
-void SwitchPushrod::setup(Pushrod *_pushrod, Switch &_start, Switch &_end)
+void SwitchPushrod::setup(Pushrod *_pushrod, Switch *_start, Switch *_end)
 {
     pushrod = _pushrod;
     start = _start;
@@ -19,27 +19,44 @@ void SwitchPushrod::setPushrod(Pushrod *_pushrod)
 {
     pushrod = _pushrod;
 }
-void SwitchPushrod::setStart(Switch &_start)
+void SwitchPushrod::setStart(Switch *_start)
 {
     start = _start;
 }
-void SwitchPushrod::setSwitch(Switch &_end)
+void SwitchPushrod::setSwitch(Switch *_end)
 {
     end = _end;
 }
 void SwitchPushrod::forward()
 {
+    steps = 0;
     pushrod->resetStep();
-    while (end.isOpen())
+    while (end->isOpen())
     {
         pushrod->forward(1);
+        steps++;
+        if (user_onStep(steps, STEP_FORWARD) == EVENT_STEP_STOP)
+        {
+            break;
+        }
     }
 }
 void SwitchPushrod::backward()
 {
+    steps = 0;
     pushrod->resetStep();
-    while (start.isOpen())
+    while (start->isOpen())
     {
         pushrod->backward(1);
+        steps++;
+        if (user_onStep(steps, STEP_BACKWARD) == EVENT_STEP_STOP)
+        {
+            break;
+        }
     }
+}
+
+void SwitchPushrod::onStep(int (*function)(int, bool))
+{
+    user_onStep = function;
 }
